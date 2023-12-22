3849b8bc55ba9b9f80eee21f405cfe82e82fb25a
zoodaa <jkj01230@gmail.com>
2023-12-22 22:18:54 +0900

    feat: BASE_API 설정

---
+++ app/apis/common/CommonApi.jsx
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import {Alert} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const BASE_API = axios.create({
+  baseURL: 'https://dev-server.4983.link',
+  responseType: 'json',
+  withCredentials: true,
+  timeout: 2000,
+});
+
+// export const API = axios.create({
+//   baseURL: 'https://dev.4983.link',
+//   responseType: 'json',
+//   withCredentials: true,
+// });
+//
+// export const handleTokenExpirationError = () => {
+//   // const navigation: NavigationProp<ParamListBase> = useNavigation();
+//
+//   Alert.alert('앗!', '로그인이 만료되었습니다.', [
+//     {
+//       text: '확인',
+//       onPress: () => {
+//         // navigation.navigate('Signin');
+//         console.log('로그인 페이지로 이동');
+//       },
+//     },
+//   ]);
+// };
+//
+// BASE_API.interceptors.request.use(async config => {
+//   const accessToken = await AsyncStorage.getItem('accessToken');
+//
+//   if (accessToken != null && accessToken !== '' && accessToken !== undefined) {
+//     config.headers.Authorization = accessToken;
+//   }
+//
+//   return config;
+// });
+//
+// BASE_API.interceptors.response.use(
+//   res => res,
+//   async err => {
+//     const {
+//       config,
+//       response: {status},
+//     } = err;
+//
+//     console.log('error ', err);
+//
+//     if (status !== 401) {
+//       return Promise.reject(err);
+//     }
+//
+//     await axios
+//       .get('https://prod.deunku.com/api/v1/token/update', {
+//         withCredentials: true,
+//       })
+//       .then(res => {
+//         AsyncStorage.setItem('accessToken', res.headers.authorization);
+//
+//         console.log('res.headers.Authorization', res.headers.authorization);
+//
+//         if (typeof res.headers.Authorization === 'string') {
+//           config.headers.Authorization = res.headers.Authorization;
+//           return axios(config);
+//         }
+//       })
+//       .catch(() => {
+//         return Promise.reject(err);
+//       });
+//
+//     return Promise.reject(err);
+//   },
+// );
