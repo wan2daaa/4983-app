857c84a73fe799abdd6ce154825de0243a2d331c
wan2daaa <wan2daaa@gmail.com>
2024-02-15 16:17:30 +0900

    fix: 채팅방 리스트 페이지에 데이터 가져올 때 useIsFocused 추가

--- app/screens/ChatList.jsx
+++ app/screens/ChatList.jsx
@@ -1,16 +1,19 @@
 import {ChatListLayout} from '@components/chat-list/chat-list-layout';
 import {getChatList} from '@/apis/chat-list/ChatListApi';
 import {useEffect, useState} from 'react';
+import {useIsFocused} from '@react-navigation/native';
 
 export const ChatList = ({navigation}) => {
   const [chatDataList, setChatDataList] = useState([]);
+
+  const isFocused = useIsFocused();
 
   useEffect(() => {
     getChatList().then(res => {
       console.log('res: ', res);
       setChatDataList(res);
     });
-  }, []);
+  }, [isFocused]);
 
   return <ChatListLayout navigation={navigation} chatDataList={chatDataList} />;
 };
